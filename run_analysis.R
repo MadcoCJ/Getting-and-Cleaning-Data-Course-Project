11389f22b92c8bd6fedcb4af50eb9beaa88d1d79
CJ <cmad08@gmail.com>
2016-04-24 21:00:48 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,7 @@
 ##assuming the Samsung Data is in your working directory
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl, destfile ="./data/Dataset.zip",method="curl")
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
